6d81ed64229b8e42aa476b8231104ea75dd258c2
Alexandre Anastassov <a.anastassov@iaea.org>
2018-04-12 18:04:55 +0200

    write add() function with tests

--- src/App.js
+++ src/App.js
@@ -1,1 +1,6 @@
 export const hello = () => 'Hello';
+
+export const add = (x, y) => { 
+    if (typeof x === 'number' && typeof y  === 'number') return x + y;
+    return null;
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { hello } from './App';
+import { hello, add } from './App';
 
 describe('hello', () => {
     it('should output hello', () => {
@@ -6,3 +6,25 @@ describe('hello', () => {
     });
 });
 
+describe('add', () => {
+    it('should add two numbers', () => {
+        expect(add(1,1)).toBe(2);
+        expect(add(1,-1)).toBe(0);
+        expect(add(0,0)).toBe(0);
+        expect(add(1,2)).toBe(3);
+        expect(add(2,2)).toBe(4);
+        expect(add(-2,-2)).toBe(-4);
+    });
+    it('should not add strings', () => {
+        expect(add(2,'2')).toBe(null);
+    });
+    it('should not add objects', () => {
+        expect(add(2, {})).toBe(null);
+    });
+    it('should add floats', () => {
+        expect(add(2, 2.2)).toBe(4.2);
+    });
+    it('should add arrays', () => {
+        expect(add(2, [])).toBe(null);
+    });
+});
